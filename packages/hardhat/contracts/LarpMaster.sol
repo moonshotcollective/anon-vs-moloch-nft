28de6050623e6a152090fe548bf8d6a1d427e16d
Jason Romero <codenamejason@outlook.com>
2021-10-02 12:11:37 -0400

    add factory contract agian lol

---
+++ packages/hardhat/contracts/LarpMaster.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.7.6;
+//SPDX-License-Identifier: MIT
+
+/// @title LarpMaster Factory Contract
+/// @author jaxcoder, ghostffcode
+/// @notice 
+/// @dev 
+contract LarpMaster {
+    
+    constructor () public {}
+}
