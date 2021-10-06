c58e430c6f311a69b56467aa89630bfc84ebd84d
Jason Romero <codenamejason@outlook.com>
2021-10-06 19:04:12 -0400

    add base contracts

---
+++ packages/hardhat/contracts/EthStatue.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @title GreatestLARP Factory Contract
+/// @author jaxcoder, ghostffcode
+/// @notice
+/// @dev
+contract EthStatue is ERC721URIStorage {
+
+    constructor() ERC721("EthStatue", "ETHSTAT") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return
+            "https://gateway.pinata.cloud/ipfs/QmdRmZ1UPSALNVuXY2mYPb3T5exn9in1AL3tsema4rY2QF/json/";
+    }
+
+    
+}
