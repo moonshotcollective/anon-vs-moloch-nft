e81ccad83139a49b1bd21174bee313a911084b9f
farque65 <fahim.farque@gmail.com>
2021-10-11 15:56:09 -0400

    Read Comic section update

--- packages/react-app/src/components/ReadComic/index.jsx
+++ packages/react-app/src/components/ReadComic/index.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 
+import anonvsmolochissue2 from "../../assets/anonvsmoloch-issue2.svg";
+
 const ReadComic = () => {
 	return (
-			<div>
-			<div className="min-w-full bg-green-dark-green">
-				<div className="space-y-6 infront m-8 pb-8">
-				  <h1 className="infront justify-center text-center text-5xl relative mt-10 text-green-teal font-spacemono">
-				  	Read Comic
-				  </h1>
-				</div>
-			</div>
-		  </div>
+<section class="text-gray-600 body-font">
+  <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div class="w-1/2">
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Read Comic
+      </h1>
+      <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+      <div class="flex justify-center">
+        <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Read</button>
+      </div>
+    </div>
+	<div class="w-1/2">
+		<div className="flex flex-grow py-2 justify-center">
+			<img className="w-1/3 z-50" src={anonvsmolochissue2} />
+		</div>
+    </div>
+  </div>
+</section>
 	)
 }
 
