d832928a8b0c4c77e02299c5222edf55e8b9571f
farque65 <fahim.farque@gmail.com>
2021-10-12 11:40:08 -0400

    Front end update

--- packages/react-app/src/components/Contract/FunctionForm.jsx
+++ packages/react-app/src/components/Contract/FunctionForm.jsx
@@ -3,13 +3,11 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
 import React, { useState } from "react";
 import { BigNumber } from "@ethersproject/bignumber";
-import { hexlify } from "@ethersproject/bytes";
 import { Row, Col, Input, Divider, Tooltip, Button } from "antd";
 import { Transactor } from "../../helpers";
 import tryToDisplay from "./utils";
 import Blockies from "react-blockies";
 const { utils } = require("ethers");
-
 
 export default function FunctionForm({ contractFunction, functionInfo, provider, gasPrice, triggerRefresh }) {
   const [form, setForm] = useState({});
@@ -20,10 +18,9 @@ export default function FunctionForm({ contractFunction, functionInfo, provider,
 
   let inputIndex = 0;
   const inputs = functionInfo.inputs.map(input => {
+    const key = functionInfo.name + "_" + input.name + "_" + input.type + "_" + inputIndex++;
 
-    const key = functionInfo.name + "_" + input.name + "_" + input.type + "_" + inputIndex++
-
-    let buttons = ""
+    let buttons = "";
     if (input.type === "bytes32") {
       buttons = (
         <Tooltip placement="right" title={"to bytes32"}>
@@ -43,9 +40,9 @@ export default function FunctionForm({ contractFunction, functionInfo, provider,
             }}
           >
             #️⃣
-            </div>
+          </div>
         </Tooltip>
-      )
+      );
     } else if (input.type === "bytes") {
       buttons = (
         <Tooltip placement="right" title={"to hex"}>
@@ -59,16 +56,16 @@ export default function FunctionForm({ contractFunction, functionInfo, provider,
                 setForm(formUpdate);
               } else {
                 const formUpdate = { ...form };
-                formUpdate[key] = utils.hexlify(utils.toUtf8Bytes(form[key]))
+                formUpdate[key] = utils.hexlify(utils.toUtf8Bytes(form[key]));
                 setForm(formUpdate);
               }
             }}
           >
             #️⃣
-            </div>
+          </div>
         </Tooltip>
-      )
-    } else if (input.type == "uint256") {
+      );
+    } else if (input.type === "uint256") {
       buttons = (
         <Tooltip placement="right" title={"* 10 ** 18"}>
           <div
@@ -76,27 +73,24 @@ export default function FunctionForm({ contractFunction, functionInfo, provider,
             style={{ cursor: "pointer" }}
             onClick={async () => {
               const formUpdate = { ...form };
-              formUpdate[key] = utils.parseEther(form[key])
+              formUpdate[key] = utils.parseEther(form[key]);
               setForm(formUpdate);
             }}
           >
             ✴️
-            </div>
+          </div>
         </Tooltip>
-      )
-    } else if (input.type == "address") {
-      const possibleAddress = form[key]&&form[key].toLowerCase&&form[key].toLowerCase().trim()
-      if(possibleAddress && possibleAddress.length==42){
+      );
+    } else if (input.type === "address") {
+      const possibleAddress = form[key] && form[key].toLowerCase && form[key].toLowerCase().trim();
+      if (possibleAddress && possibleAddress.length === 42) {
         buttons = (
           <Tooltip placement="right" title={"blockie"}>
             <Blockies seed={possibleAddress} scale={3} />
           </Tooltip>
-        )
+        );
       }
     }
-
-
-
 
     return (
       <div style={{ margin: 2 }} key={key}>
@@ -106,7 +100,7 @@ export default function FunctionForm({ contractFunction, functionInfo, provider,
           autoComplete="off"
           value={form[key]}
           name={key}
-          onChange={(event) => {
+          onChange={event => {
             const formUpdate = { ...form };
             formUpdate[event.target.name] = event.target.value;
             setForm(formUpdate);
@@ -114,7 +108,7 @@ export default function FunctionForm({ contractFunction, functionInfo, provider,
           suffix={buttons}
         />
       </div>
-    )
+    );
   });
 
   const txValueInput = (
@@ -132,8 +126,8 @@ export default function FunctionForm({ contractFunction, functionInfo, provider,
                     type="dashed"
                     style={{ cursor: "pointer" }}
                     onClick={async () => {
-                      let floatValue = parseFloat(txValue)
-                      if(floatValue) setTxValue("" + floatValue * 10 ** 18);
+                      let floatValue = parseFloat(txValue);
+                      if (floatValue) setTxValue("" + floatValue * 10 ** 18);
                     }}
                   >
                     ✳️
@@ -150,7 +144,7 @@ export default function FunctionForm({ contractFunction, functionInfo, provider,
                     }}
                   >
                     #️⃣
-                </div>
+                  </div>
                 </Tooltip>
               </Col>
             </Row>
@@ -164,7 +158,12 @@ export default function FunctionForm({ contractFunction, functionInfo, provider,
     inputs.push(txValueInput);
   }
 
-  const buttonIcon = functionInfo.type === "call" ? <Button style={{ marginLeft: -32 }}>Read📡</Button> : <Button style={{ marginLeft: -32 }}>Send💸</Button>;
+  const buttonIcon =
+    functionInfo.type === "call" ? (
+      <Button style={{ marginLeft: -32 }}>Read📡</Button>
+    ) : (
+      <Button style={{ marginLeft: -32 }}>Send💸</Button>
+    );
   inputs.push(
     <div style={{ cursor: "pointer", margin: 2 }} key={"goButton"}>
       <Input
@@ -178,27 +177,27 @@ export default function FunctionForm({ contractFunction, functionInfo, provider,
             style={{ width: 50, height: 30, margin: 0 }}
             type="default"
             onClick={async () => {
-              let innerIndex = 0
-              const args = functionInfo.inputs.map((input) => {
-                const key = functionInfo.name + "_" + input.name + "_" + input.type + "_" + innerIndex++
-                let value = form[key]
-                if(input.baseType=="array"){
-                  value = JSON.parse(value)
-                } else if(input.type === "bool"){
-                  if(value==='true' || value==='1' || value ==="0x1"|| value ==="0x01"|| value ==="0x0001"){
+              let innerIndex = 0;
+              const args = functionInfo.inputs.map(input => {
+                const key = functionInfo.name + "_" + input.name + "_" + input.type + "_" + innerIndex++;
+                let value = form[key];
+                if (input.baseType === "array") {
+                  value = JSON.parse(value);
+                } else if (input.type === "bool") {
+                  if (value === "true" || value === "1" || value === "0x1" || value === "0x01" || value === "0x0001") {
                     value = 1;
-                  }else{
+                  } else {
                     value = 0;
                   }
                 }
-                return value
+                return value;
               });
 
-              let result
-              if(functionInfo.stateMutability === "view"||functionInfo.stateMutability === "pure"){
-                const returned = await contractFunction(...args)
+              let result;
+              if (functionInfo.stateMutability === "view" || functionInfo.stateMutability === "pure") {
+                const returned = await contractFunction(...args);
                 result = tryToDisplay(returned);
-              }else{
+              } else {
                 const overrides = {};
                 if (txValue) {
                   overrides.value = txValue; // ethers.utils.parseEther()
@@ -206,12 +205,10 @@ export default function FunctionForm({ contractFunction, functionInfo, provider,
                 // Uncomment this if you want to skip the gas estimation for each transaction
                 // overrides.gasLimit = hexlify(1200000);
 
-
                 // console.log("Running with extras",extras)
                 const returned = await tx(contractFunction(...args, overrides));
                 result = tryToDisplay(returned);
               }
-
 
               console.log("SETTING RESULT:", result);
               setReturnValue(result);
