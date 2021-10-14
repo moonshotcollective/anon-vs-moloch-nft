38bb82a925371d38597b4b280f174e6430b55082
Bliss Da Sailor <meremezeukah@gmail.com>
2021-10-14 10:43:51 -0500

    formatting

--- packages/react-app/tailwind.config.js
+++ packages/react-app/tailwind.config.js
@@ -1,4 +1,4 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
@@ -6,44 +6,50 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'intro-background': "url('/src/assets/intro-background.png')",
-        'nft-background': "url('/src/assets/nft-background.svg')",
-        'nft3-background': "url('/src/assets/nft3-background.png')"
+        "intro-background": "url('/src/assets/intro-background.png')",
+        "nft-background": "url('/src/assets/nft-background.svg')",
+        "nft3-background": "url('/src/assets/nft3-background.png')",
       },
       colors: {
         orange: colors.orange,
         green: {
-          050: '#6dc5a040',
-          'dark-green': '#337062',
-          'teal': '#2CAE92',
-          'light-green': '#E2F3EC',
+          "050": "#6dc5a0",
+          "dark-green": "#337062",
+          teal: "#2CAE92",
+          header: "#6AC59F",
+          "light-green": "#E2F3EC",
+          imgBg: "#9ED5AA",
+        },
+        purple: {
+          overlay: "#240871",
+          imgText: "#8C65F7",
         },
         brown: {
-          'dark-brown': '#262626',
+          "dark-brown": "#262626",
         },
         gray: {
-          050: '#FFFFFF',
-          1000: '#343a39',
+          "050": "#FFFFFF",
+          1000: "#343a39",
         },
         red: {
-          'bloodred': '#ea1e5047',
-          'soldout': '#EB1E50',
-        }
+          bloodred: "#ea1e5047",
+          soldout: "#EB1E50",
+        },
       },
     },
     fontFamily: {
-      'spacemono': ['Space Mono'],
-      'librefranklin': ['Libre Franklin']
+      spacemono: ["Space Mono"],
+      librefranklin: ["Libre Franklin"],
     },
     minHeight: {
-      '0': '0',
-      '1/4': '25%',
-      '1/2': '50%',
-      '3/4': '75%',
-      'full': '100%',
-      'intro': '890px',
-      'intro-mobile': '450px',
-     }
+      0: "0",
+      "1/4": "25%",
+      "1/2": "50%",
+      "3/4": "75%",
+      full: "100%",
+      intro: "890px",
+      "intro-mobile": "450px",
+    },
   },
   variants: {
     extend: {},
