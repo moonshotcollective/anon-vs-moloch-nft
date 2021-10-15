2018b1f038dc799a836c991c8c0d39d404771062
Bliss Da Sailor <meremezeukah@gmail.com>
2021-10-14 22:04:00 -0500

    update deployment script

--- packages/hardhat/deploy/00_deploy_greatestLARP_contract.js
+++ packages/hardhat/deploy/00_deploy_greatestLARP_contract.js
@@ -1,22 +1,22 @@
 // deploy/00_deploy_your_contract.js
 
-const { ethers } = require("hardhat");
+const { ethers } = require('hardhat')
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
-  const { deploy } = deployments;
-  const { deployer } = await getNamedAccounts();
+  const { deploy } = deployments
+  const { deployer } = await getNamedAccounts()
 
   // deploy ERC-721 Tokens
-  const eBot = await deploy("EthBot", { from: deployer, log: true });
-  const mBot = await deploy("MolochBot", { from: deployer, log: true });
-  const eStatue = await deploy("EthBotStatue", { from: deployer, log: true });
-  const mStatue = await deploy("MolochBotStatue", {
+  const eBot = await deploy('EthBot', { from: deployer, log: true })
+  const mBot = await deploy('MolochBot', { from: deployer, log: true })
+  const eStatue = await deploy('EthBotStatue', { from: deployer, log: true })
+  const mStatue = await deploy('MolochBotStatue', {
     from: deployer,
     log: true,
-  });
+  })
 
   // deploy factory contract
-  const factory = await deploy("GreatestLARP", {
+  const factory = await deploy('GreatestLARP', {
     // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
     from: deployer,
     args: [
@@ -24,22 +24,30 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
       [eStatue.address, mStatue.address],
       [3, 3], // Digital threshold
       [2, 2], // Physical threshold
-      ethers.utils.parseUnits("0.0033"), // Digital
-      ethers.utils.parseUnits("3"), // Pysical
+      ethers.utils.parseUnits('0.0033'), // Digital
+      ethers.utils.parseUnits('3'), // Pysical
     ],
     log: true,
-  });
+  })
 
   // load token contracts
-  const EthBot = await ethers.getContract("EthBot", deployer);
-  const MolochBot = await ethers.getContract("MolochBot", deployer);
+  const EthBot = await ethers.getContract('EthBot', deployer)
+  const MolochBot = await ethers.getContract('MolochBot', deployer)
+  const EthBotStatue = await ethers.getContract('EthBotStatue', deployer)
+  const MolochBotStatue = await ethers.getContract('MolochBotStatue', deployer)
 
   // transfer ownerships to the factory contract
-  const EthBotTransfer = await EthBot.transferOwnership(factory.address);
-  await EthBotTransfer.wait(1);
+  const EthBotTransfer = await EthBot.transferOwnership(factory.address)
+  await EthBotTransfer.wait(1)
 
-  const MolochBotTransfer = await MolochBot.transferOwnership(factory.address);
-  await MolochBotTransfer.wait(1);
-};
+  const MolochBotTransfer = await MolochBot.transferOwnership(factory.address)
+  await MolochBotTransfer.wait(1)
 
-module.exports.tags = ["GreatestLARP", "EthBot", "MolochBot"];
+  const EthBotStatueTransfer = await EthBotStatue.transferOwnership(factory.address)
+  await EthBotStatueTransfer.wait(1)
+
+  const MolochBotStatueTransfer = await MolochBotStatue.transferOwnership(factory.address)
+  await MolochBotStatueTransfer.wait(1)
+}
+
+module.exports.tags = ['GreatestLARP', 'EthBot', 'MolochBot']
