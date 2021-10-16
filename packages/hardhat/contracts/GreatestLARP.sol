2ce8ed0cc654cf4e838f6c86cb0a4090e2eadfd5
Jason Romero <codenamejason@outlook.com>
2021-10-16 11:45:56 -0400

    update whomp func to take level

--- packages/hardhat/contracts/GreatestLARP.sol
+++ packages/hardhat/contracts/GreatestLARP.sol
@@ -100,15 +100,18 @@ contract GreatestLARP is Ownable {
 
     /// @dev A function that can be called from Etherscan to lower
     ///      the price of the items by 10%.
-    function whompwhomp()
+    function whompwhomp(uint256 _level)
+        isValidLevel(_level)
         public
         onlyOwner
     {
-        // lower the price 10% of item
-        tokenMap[1].price = tokenMap[1].price.sub(tokenMap[1].price.div(100).mul(10));
-        tokenMap[2].price = tokenMap[1].price.sub(tokenMap[1].price.div(100).mul(10));
-        statueMap[1].price = tokenMap[1].price.sub(tokenMap[1].price.div(100).mul(10));
-        statueMap[2].price = tokenMap[1].price.sub(tokenMap[1].price.div(100).mul(10));
+        if(_level == 1) {
+            tokenMap[_level].price = tokenMap[_level].price.sub(tokenMap[_level].price.div(100).mul(10));
+            statueMap[_level].price = statueMap[_level].price.sub(statueMap[_level].price.div(100).mul(10));
+        } else if (_level == 2) {
+            tokenMap[_level].price = tokenMap[_level].price.sub(tokenMap[_level].price.div(100).mul(10));
+            statueMap[_level].price = statueMap[_level].price.sub(statueMap[_level].price.div(100).mul(10));
+        }
     }
 
     /// @dev returns the details for a Bot level
@@ -117,13 +120,13 @@ contract GreatestLARP is Ownable {
         view
         returns (
             uint256 price,
-            uint256 thresholdBots,
+            uint256 threshold,
             uint256 totalSupply,
             address tokenAddress
         )
     {
         price = tokenMap[level].price;
-        thresholdBots = tokenMap[level].thresholdBots;
+        threshold = tokenMap[level].thresholdBots;
         totalSupply = tokenMap[level].totalSupply;
         tokenAddress = tokenMap[level].tokenAddress;
     }
@@ -134,13 +137,13 @@ contract GreatestLARP is Ownable {
         view
         returns (
             uint256 price,
-            uint256 thresholdStatues,
+            uint256 threshold,
             uint256 totalSupply,
             address tokenAddress
         )
     {
         price = statueMap[level].price;
-        thresholdStatues = statueMap[level].thresholdStatues;
+        threshold = statueMap[level].thresholdStatues;
         totalSupply = statueMap[level].totalSupply;
         tokenAddress = statueMap[level].tokenAddress;
     }
