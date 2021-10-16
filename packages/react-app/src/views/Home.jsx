0e69132cce772acc9c8b4dba959cc486a96d5ab6
Bliss Da Sailor <meremezeukah@gmail.com>
2021-10-16 16:43:37 -0400

    implement nav

--- packages/react-app/src/views/Home.jsx
+++ packages/react-app/src/views/Home.jsx
@@ -1,77 +1,26 @@
-import { Image, Space, Row, Col } from "antd";
 import React from "react";
-import navbarheadlogo from "../assets/navbarheadlogo.svg";
-import { Account, EthbotLearn, EthbotProgress, Faq, Footer, ReadComic } from "../components";
-import { Button } from "../themed-components";
+import { EthbotLearn, EthbotProgress, Faq, Footer, ReadComic } from "../components";
+import { Button, Nav } from "../themed-components";
 
-function Home({
-  faucetHint,
-  faucetAvailable,
-  address,
-  localProvider,
-  userSigner,
-  mainnetProvider,
-  price,
-  web3Modal,
-  loadWeb3Modal,
-  logoutOfWeb3Modal,
-  blockExplorer,
-  gasPrice,
-  networkDisplay,
-  ...props
-}) {
+function Home(props) {
   return (
     <div className="Home">
-      <div class="flex flex-row p-5 w-full">
-        <a href="/" target="_blank" rel="noopener noreferrer" className="float-left">
-          <Image preview={false} className="h-24" src={navbarheadlogo} />
-        </a>
-        <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-          <a href="/" class="mr-10 hover:text-gray-900 text-green-dark-green">
-            Explore Editions
-          </a>
-          <a href="/" class="mr-10 hover:text-gray-900 text-green-dark-green">
-            How it Works?
-          </a>
-          <a href="/" class="mr-10 hover:text-gray-900 text-green-dark-green">
-            Community
-          </a>
-          <a href="/" class="mr-10 hover:text-gray-900 text-green-dark-green">
-            About
-          </a>
-        </nav>
-        <Space>
-          <span>{faucetHint}</span>
-          <Account
-            address={address}
-            localProvider={localProvider}
-            userSigner={userSigner}
-            mainnetProvider={mainnetProvider}
-            price={price}
-            web3Modal={web3Modal}
-            loadWeb3Modal={loadWeb3Modal}
-            logoutOfWeb3Modal={logoutOfWeb3Modal}
-            blockExplorer={blockExplorer}
-            extra={networkDisplay}
-          />
-        </Space>
-      </div>
-
+      <Nav {...props} />
 
       <div className=" items-center justify-center bg-green-dark-green flex flex-col py-20">
-          <h1 className="title-font sm:text-6xl text-5xl mb-4 text-green-teal font-spacemono">
-            The Greatest Larp has begun.
-          </h1>
-          <p className="mb-8 leading-relaxed font-librefranklin text-white">Are You Ready?</p>
-            <Button
-              onClick={() => {
-                window.location = "/get-started";
-              }}
-              className="z-50 infront text-white bg-green-teal border-0 py-2 px-6 rounded text-lg font-spacemono"
-            >
-              Join us on this fight
-              {/* <a className="text-white hover:text-gray-900" href="/get-started">Join us on this fight</a> */}
-            </Button>
+        <h1 className="title-font sm:text-6xl text-5xl mb-4 text-green-teal font-spacemono">
+          The Greatest Larp has begun.
+        </h1>
+        <p className="mb-8 leading-relaxed font-librefranklin text-white">Are You Ready?</p>
+        <Button
+          onClick={() => {
+            window.location = "/get-started";
+          }}
+          className="z-50 infront text-white bg-green-teal border-0 py-2 px-6 rounded text-lg font-spacemono"
+        >
+          Join us on this fight
+          {/* <a className="text-white hover:text-gray-900" href="/get-started">Join us on this fight</a> */}
+        </Button>
       </div>
 
       {/* A progress attribute can be passed to show different stages of funding */}
