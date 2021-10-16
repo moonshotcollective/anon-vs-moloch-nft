8d271f586e8441960708e8325b0d52f4fcf71f98
farque65 <fahim.farque@gmail.com>
2021-10-16 16:49:06 -0400

    Updated Home View

--- packages/react-app/src/views/Home.jsx
+++ packages/react-app/src/views/Home.jsx
@@ -1,4 +1,4 @@
-import { Image, Space, Row, Col } from "antd";
+import { Image, Space } from "antd";
 import React from "react";
 import navbarheadlogo from "../assets/navbarheadlogo.svg";
 import { Account, EthbotLearn, EthbotProgress, Faq, Footer, ReadComic } from "../components";
@@ -65,7 +65,7 @@ function Home({
               onClick={() => {
                 window.location = "/get-started";
               }}
-              className="z-50 infront text-white bg-green-teal border-0 py-2 px-6 rounded text-lg font-spacemono"
+              className="text-white bg-green-teal border-0 py-2 px-6 rounded text-lg font-spacemono"
             >
               Join us on this fight
               {/* <a className="text-white hover:text-gray-900" href="/get-started">Join us on this fight</a> */}
