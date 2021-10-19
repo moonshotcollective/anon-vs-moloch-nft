63414e656e12353a77cd4bc684f6076c398d602e
Jason Romero <codenamejason@outlook.com>
2021-10-19 13:34:36 -0400

    update to zora link

--- packages/react-app/src/views/GetStarted/FinalBattle.jsx
+++ packages/react-app/src/views/GetStarted/FinalBattle.jsx
@@ -2,30 +2,33 @@ import React from "react";
 import FinBattle from "../../assets/mint/finalBattle.png";
 import { Button } from "../../themed-components";
 
-function FinalBattle({ goToPrevStep,goToNextStep }) {
+function FinalBattle({ goToPrevStep, goToNextStep }) {
   return (
     <>
       <div className="mb-8 flex flex-1 flex-col justify-center">
         <div className="max-w-md">
-        <a className="font-normal text-green-header" onClick={goToPrevStep} href="#">&lt;&lt; back to Level 3</a>
-          <h1 className="text-4xl mb-4 font-normal text-green-header font-spacemono">
-            Level 4
-          </h1>
-          <h1 className="text-2xl mb-8 font-normal text-green-header font-spacemono">
-            The Final Battle {">>"}
-          </h1>
+          <a className="font-normal text-green-header" onClick={goToPrevStep} href="#">
+            &lt;&lt; back to Level 3
+          </a>
+          <h1 className="text-4xl mb-4 font-normal text-green-header font-spacemono">Level 4</h1>
+          <h1 className="text-2xl mb-8 font-normal text-green-header font-spacemono">The Final Battle {">>"}</h1>
 
           <div className="text-lg mb-6">
-            Moloch has been weakened, and our coordination is as strong as ever.  Can Ethereum truly be the substrate for human coordination that sets Moloch back for a generation?  Will we choose to coordinate?  Or will we descend into darkness?
-            <br/> 
-            <br/> 
-            The Greatest LARP is almost over.  We've learned that we can build coordination mechanisms, but at the end of the day -- that all coordination is a choice.  It's up to you, Anon.  
-            <br/> 
+            Moloch has been weakened, and our coordination is as strong as ever. Can Ethereum truly be the substrate for
+            human coordination that sets Moloch back for a generation? Will we choose to coordinate? Or will we descend
+            into darkness?
+            <br />
+            <br />
+            The Greatest LARP is almost over. We've learned that we can build coordination mechanisms, but at the end of
+            the day -- that all coordination is a choice. It's up to you, Anon.
+            <br />
           </div>
 
           <div>
             {/* Image Title */}
-            <h3 className="font-spacemono text-purple-imgText font-semibold text-lg">The Final Battle is won when this 1/1 NFT is sold.</h3>
+            <h3 className="font-spacemono text-purple-imgText font-semibold text-lg">
+              The Final Battle is won when this 1/1 NFT is sold.
+            </h3>
 
             {/* Button */}
             <Button
@@ -33,22 +36,21 @@ function FinalBattle({ goToPrevStep,goToNextStep }) {
               className="border-2 border-green-header text-green-header hover:bg-green-dark-green mb-2"
               padding={10}
               onClick={() => {
-                window.open("https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/51825607130456865052072540421054875649262172737660879203026539353253980667905/");
+                window.open("https://zora.co/collections/zora/5725");
               }}
             >
-              View Open Sea Auction
+              View Zora Auction
             </Button>
             <div className="text-red-500">(1/1 Ultra rare piece)</div>
           </div>
-          <div>
-          </div>
+          <div></div>
         </div>
       </div>
 
       <div className="flex flex-1 justify-center items-center relative">
-        <a href="https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/51825607130456865052072540421054875649262172737660879203026539353253980667905/" target="_blank">
+        <a href="https://zora.co/collections/zora/5725" target="_blank" rel="noreferrer">
           <img src={FinBattle} alt="final battle" />
-          </a>
+        </a>
       </div>
     </>
   );
