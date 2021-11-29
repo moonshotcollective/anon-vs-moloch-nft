b17d230eac2a9b4669e79c9a696ccc438986c997
farque65 <fahim.farque@gmail.com>
2021-11-28 21:22:17 -0500

    Updated Coming Soon Page

--- packages/react-app/src/views/GetStarted/ComingSoon.jsx
+++ packages/react-app/src/views/GetStarted/ComingSoon.jsx
@@ -10,37 +10,40 @@ function ComingSoon({ goToNextStep }) {
 
     return (
       <>
-        <div class="text-center">
+        <div class="text-center font-spacemono container mx-auto">
           <h1 className="text-2xl text-green-header font-spacemono underline font-bold">
             Countdown to the final showdown auction
           </h1>
-          <div className="flex flex-row items-end lg:ml-20">
-            {/* days */}
-            <div className="flex flex-col items-center">
-              <span className="text-gray-400 font-spacemono">days</span>
-              <h2 className={classnames("mt-4 m-0 p-0 font-spacemono", largeTextClass)}>{days}</h2>
-            </div>
-            <span className={classnames(largeTextClass, "mx-5")}>:</span>
-            {/* hours */}
-            <div className="flex flex-col items-center">
-              <span className="text-gray-400 font-spacemono">hours</span>
-              <h2 className={classnames("mt-4 m-0 p-0 font-spacemono", largeTextClass)}>{hours}</h2>
-            </div>
-            <span className={classnames(largeTextClass, "mx-5")}>:</span>
-            {/* minutes */}
-            <div className="flex flex-col items-center">
-              <span className="text-gray-400 font-spacemono">minutes</span>
-              <h2 className={classnames("mt-4 m-0 p-0 font-spacemono", largeTextClass)}>{minutes}</h2>
-            </div>
-            <span className={classnames(largeTextClass, "mx-5")}>:</span>
-            {/* seconds */}
-            <div className="flex flex-col items-center">
-              <span className="text-gray-400 font-spacemono">seconds</span>
-              <h2 className={classnames("mt-4 m-0 p-0 font-spacemono", largeTextClass)}>{seconds}</h2>
+          <div className="container p-4 sm:w-1/2 lg:w-1/4 w-1/2">
+            <div className="flex flex-row items-end">
+              {/* days */}
+              <div className="flex flex-col items-center">
+                <span className="text-gray-400 font-spacemono">days</span>
+                <h2 className={classnames("mt-4 m-0 p-0 font-spacemono", largeTextClass)}>{days}</h2>
+              </div>
+              <span className={classnames(largeTextClass, "mx-5")}>:</span>
+              {/* hours */}
+              <div className="flex flex-col items-center">
+                <span className="text-gray-400 font-spacemono">hours</span>
+                <h2 className={classnames("mt-4 m-0 p-0 font-spacemono", largeTextClass)}>{hours}</h2>
+              </div>
+              <span className={classnames(largeTextClass, "mx-5")}>:</span>
+              {/* minutes */}
+              <div className="flex flex-col items-center">
+                <span className="text-gray-400 font-spacemono">minutes</span>
+                <h2 className={classnames("mt-4 m-0 p-0 font-spacemono", largeTextClass)}>{minutes}</h2>
+              </div>
+              <span className={classnames(largeTextClass, "mx-5")}>:</span>
+              {/* seconds */}
+              <div className="flex flex-col items-center">
+                <span className="text-gray-400 font-spacemono">seconds</span>
+                <h2 className={classnames("mt-4 m-0 p-0 font-spacemono", largeTextClass)}>{seconds}</h2>
+              </div>
             </div>
           </div>
         </div>
-        <div className='player-wrapper mt-20'>
+
+        <div className='player-wrapper'>
           <ReactPlayer
           url= 'videos/botVideoComingSoon.mp4'
           width='100%'
@@ -48,13 +51,15 @@ function ComingSoon({ goToNextStep }) {
           controls = {true}
           />
         </div>
-        <div class="w-3/4 mx-auto text-center">
-          <p class="leading-relaxed text-lg mt-20 text-3xl font-bold text-green-header font-spacemono">
+        <div class="w-3/4 mx-auto text-center mt-20">
+          <a href=" https://www.addevent.com/event/Qu10410679" class="leading-relaxed text-lg mt-20 text-3xl font-bold text-green-header font-spacemono underline">
             Join us for the kickoff party Dec 6!
-          </p>
-          <Button disabled={!completed} onClick={goToNextStep}>
-            Join here
-          </Button>
+          </a>
+          <div className="mt-10">
+            <Button disabled={!completed} onClick={goToNextStep}>
+              Join here
+            </Button>
+          </div>
           <p>(You want to do it!)</p>
         </div>
        
@@ -64,10 +69,10 @@ function ComingSoon({ goToNextStep }) {
 
   return (
     <div className="grid grid-cols-1">
-      <div className="flex flex-wrap">
+      <div className="flex flex-wrap font-spacemono">
 
-        <div className="flex flex-wrap w-1/2 p-20 sm:p-6">
-          <div className="max-w-full flex flex-col ml-6">
+        <div className="flex flex-wrap w-1/3 p-20 sm:p-6 mr-16">
+          <div className="max-w-full flex flex-col">
             <h1 className="text-2xl font-normal text-green-header font-spacemono underline font-bold">
               We've come a long way together
             </h1>
@@ -78,26 +83,26 @@ function ComingSoon({ goToNextStep }) {
             </ul>
             <p className="ml-6">[raising $330,000 for public goods so far]</p>
             <br/>
-            <p className="text-5xl font-normal text-purple-final font-spacemono font-bold">
+            <p className="mt-16 text-5xl font-normal text-purple-final font-spacemono font-bold">
               Now we must beat the final Moloch boss together.
             </p>
-            <div className="mt-6 mb-6">
+            <div className="mt-10 mb-6">
               <h1 className="text-2xl font-normal text-green-header font-spacemono underline font-bold">
                 It's all coordination
               </h1>
             </div>
-            <div className="mt-2">
+            <div className="mt-10">
               <p className="text-lg font-normal text-green-header font-spacemono">
                 On December 6th, the <span className="text-purple-final font-bold" >ETHBot vs. Moloch</span> <span className="text-orange-final font-bold" >GitcoinParty Crowdfund </span> will open. By crowdfunding 50 ETH as a community, we will hit the reserve,
                 guaranteeing a chance for ETHBot to take out Moloch once and for all.
               </p>
             </div>
-            <div className="mt-2">
+            <div className="mt-10">
               <p className="text-lg font-normal text-green-header font-spacemono">
                 By participating in the winning <span className="font-bold" >GitcoinParty Crowdfund</span> you will own a percentage of the <span className="font-bold" >1 of 1</span> <span className="text-orange-final font-bold" >Final Battle NFT</span>.
               </p>
             </div>
-            <div className="mt-2">
+            <div className="mt-10">
               <p className="text-lg font-normal text-green-header font-spacemono mt-2">
                 All proceeds fund public goods (Always has been).
               </p>
